1adcdee320707bc8831d2f008a12ba67d961ce96
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-07-22 21:03:36 +0800

    Add parse_gdc_uuid

---
+++ R/parse_gdc_uuid.R
@@ -0,0 +1,25 @@
+#' Parse Metadata from GDC Portal File UUID
+#'
+#' @param x a GDC manifest file or a vector of file UUIDs.
+#'
+#' @return a `data.frame`
+#' @importFrom httr GET content
+#' @export
+#'
+#' @examples
+#' parse_gdc_file_uuid("874e71e0-83dd-4d3e-8014-10141b49f12c")
+#' parse_gdc_file_uuid(
+#'   c("874e71e0-83dd-4d3e-8014-10141b49f12c",
+#'   "2c16506f-1110-4d60-81e3-a85233c79909")
+#' )
+parse_gdc_file_uuid <- function(x) {
+  if (length(x) == 1 && file.exists(x)) {
+    x <- data.table::fread(x)[[1]]
+  }
+  message("Querying info from GDC portal API: https://api.gdc.cancer.gov")
+
+  x <- sprintf("https://api.gdc.cancer.gov/files/%s?format=tsv", x)
+  data.table::rbindlist(lapply(x, function(x) {
+    suppressMessages(content(GET(x), type = "text/tab-separated-values", encoding = "UTF-8"))
+  }), use.names = TRUE, fill = TRUE)
+}
